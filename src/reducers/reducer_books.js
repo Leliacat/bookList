94d70f1f7691b27bc2f80d34c29778bb2ea19a4b
Leliacat <wattezlelia@yahoo.fr>
2018-08-31 18:56:01 +0200

    first commit!yeah I know, I know: I should have done it earlier

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title: 'The lost symbol', pages: 67},
+        {title: 'Doctor Sleep', pages: 89},
+        {title: 'From the dust returned', pages: 79},
+        {title: '22/11/63', pages: 456}
+    ]
+}
