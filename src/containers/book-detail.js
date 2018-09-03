66f4737ad80b0f4192100d3dba783353debac732
Leliacat <wattezlelia@yahoo.fr>
2018-09-03 10:10:55 +0200

    more book info

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -5,12 +5,12 @@ import { connect } from 'react-redux';
 class BookDetail extends Component {
     render(){
         if(!this.props.book){
-            return <div className='book_details'><h3>Select a book to get started</h3></div>
+            return <div className='book_details'><h4>Select a book to get started</h4></div>
         }
         return(
             <div className='book_details'>
                 <h6>Details for:</h6>
-                <div><h3>{this.props.book.title}</h3></div>
+                <div><h3>{this.props.book.title}</h3>({this.props.book.date})</div>
                 <div>Pages: {this.props.book.pages}</div>
                 <div>Author: {this.props.book.author}</div>
                 <img className='book_img' src={this.props.book.img} />
